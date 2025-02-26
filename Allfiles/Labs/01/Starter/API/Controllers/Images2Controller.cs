afc0ad8c06236828f7c968ad4b97847487772690
vikesh-bansal <vikesh.bansal1@gmail.com>
2025-02-26 13:41:31 +0530

    generate thumbnai

--- Allfiles/Labs/01/Starter/API/Controllers/Images2Controller.cs
+++ Allfiles/Labs/01/Starter/API/Controllers/Images2Controller.cs
@@ -3,9 +3,11 @@ using Azure.Storage.Blobs.Models;
 using Azure.Storage.Sas;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -55,5 +57,51 @@ namespace Api.Controllers
             Console.Out.WriteLine("Got Images");
             return Ok(results);
         }
+        [HttpGet("thumbnail")]
+        public async Task<ActionResult<IEnumerable<string>>> GetThumbnails()
+        {
+            BlobContainerClient blobContainerClient = await GetCloudBlobContainer(_options.FullImageContainerName);
+
+            BlobClient blobClient;
+            BlobSasBuilder blobSasBuilder;
+
+            List<string> results = new List<string>();
+            await foreach (BlobItem blobItem in blobContainerClient.GetBlobsAsync())
+            {
+                blobClient = blobContainerClient.GetBlobClient(blobItem.Name);
+                blobSasBuilder = new BlobSasBuilder()
+                {
+                    BlobContainerName = _options.FullImageContainerName,
+                    BlobName = blobItem.Name,
+                    ExpiresOn = DateTime.UtcNow.AddMinutes(5),
+                    Protocol = SasProtocol.Https
+                };
+                blobSasBuilder.SetPermissions(BlobSasPermissions.Read);
+
+                results.Add(blobClient.GenerateSasUri(blobSasBuilder).AbsoluteUri);
+            }
+            Console.Out.WriteLine("Got Images");
+            return Ok(results);
+        }
+        public async Task<ActionResult> Post()
+        {
+            Stream image = Request.Body;
+            BlobContainerClient containerClient = await GetCloudBlobContainer(_options.FullImageContainerName);
+            string blobName = Guid.NewGuid().ToString().ToLower().Replace("-", string.Empty);
+            BlobClient blobClient=containerClient.GetBlobClient(blobName);
+            await blobClient.UploadAsync(image);
+            return Created(blobClient.Uri, null);
+        }
+        [HttpPost("thumbnail")]
+        public async Task<ActionResult> PostThumbnail()
+        {
+            Stream image = Request.Body;
+            BlobContainerClient containerClient = await GetCloudBlobContainer(_options.ThumbnailImageContainerName);
+            string blobName = Guid.NewGuid().ToString().ToLower().Replace("-", string.Empty);
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+            await blobClient.UploadAsync(image);
+            return Created(blobClient.Uri, null);
+        }
+
     }
 }
--- Allfiles/Labs/01/Starter/Web/Pages/product.cshtml.cs
+++ Allfiles/Labs/01/Starter/Web/Pages/product.cshtml.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -22,6 +24,10 @@ namespace Web.Pages
         }
         [BindProperty]  
         public List<string> ImageList { get; private set; }
+
+        [BindProperty]
+        public List<string> ThumbnailList { get; private set; }
+
         [BindProperty]
         public IFormFile Upload { get; set; }
         public async Task OnGetAsync()
@@ -31,21 +37,82 @@ namespace Web.Pages
             IEnumerable<string> imagesList=JsonConvert.DeserializeObject<IEnumerable<string>>(imagesJson);
 
             this.ImageList = imagesList.ToList<string>();
+
+
+            var thumbImageUrl = _options.ApiUrl+ "/thumbnail";
+            string thumbsJson = await _httpClient.GetStringAsync(thumbImageUrl);
+            ThumbnailList = JsonConvert.DeserializeObject<IEnumerable<string>>(thumbsJson).ToList(); 
+            this.ImageList = imagesList.ToList<string>();
+
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if(Upload!=null && Upload.Length > 0)
+            if (Upload != null && Upload.Length > 0)
             {
                 var imageUrl = _options.ApiUrl;
-                using (var image = new StreamContent(Upload.OpenReadStream()))
-                {
-                    image.Headers.ContentType = new MediaTypeHeaderValue(Upload.ContentType);
-                    var response = await _httpClient.PostAsync(imageUrl, image);
 
-                };
+                // Read image into memory first to avoid stream conflicts
+                using (var memoryStream = new MemoryStream())
+                {
+                    await Upload.CopyToAsync(memoryStream);
+                    memoryStream.Position = 0;
+
+                    // Upload Original Image
+                    using (var imageStream = new MemoryStream(memoryStream.ToArray()))
+                    using (var imageContent = new StreamContent(imageStream))
+                    {
+                        imageContent.Headers.ContentType = new MediaTypeHeaderValue(Upload.ContentType);
+                        await _httpClient.PostAsync(imageUrl, imageContent);
+                    }
+
+                    // Generate Thumbnail
+                    var thumbImageUrl = _options.ApiUrl;
+                    using (var sourceImage = new Bitmap(memoryStream))
+                    using (var objBitMap = new Bitmap(200, 200))
+                    using (var objGraphics = Graphics.FromImage(objBitMap))
+                    {
+                        objGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                        objGraphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+                        // Maintain Aspect Ratio
+                        int newWidth, newHeight;
+                        float aspectRatio = (float)sourceImage.Width / sourceImage.Height;
+                        if (aspectRatio > 1)
+                        {
+                            newWidth = 200;
+                            newHeight = (int)(200 / aspectRatio);
+                        }
+                        else
+                        {
+                            newHeight = 200;
+                            newWidth = (int)(200 * aspectRatio);
+                        }
+
+                        objGraphics.DrawImage(sourceImage, (200 - newWidth) / 2, (200 - newHeight) / 2, newWidth, newHeight);
+
+                        // Convert Thumbnail to Stream and Upload
+                        using (var thumbStream = new MemoryStream())
+                        {
+                            objBitMap.Save(thumbStream, System.Drawing.Imaging.ImageFormat.Png);
+                            thumbStream.Position = 0;
+
+                            using (var thumbContent = new StreamContent(thumbStream))
+                            {
+                                thumbContent.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+                                await _httpClient.PostAsync(thumbImageUrl, thumbContent);
+                            }
+                        }
+                    }
+                }
             }
+
             return RedirectToPage("/product");
+        }
+
+        public bool ThumbnailCallBack()
+        {
+            return false;
         }
     }
 }
