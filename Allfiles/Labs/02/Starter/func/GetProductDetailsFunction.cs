9ff9a7a261c488d2ca1ea32a04b552b509a6f58b
Vikesh <vikesh@essorsolutions.com>
2025-03-08 18:11:54 +0530

    Get Product detail json

--- Allfiles/Labs/02/Starter/func/GetProductDetailsFunction.cs
+++ Allfiles/Labs/02/Starter/func/GetProductDetailsFunction.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Net;
 
 namespace func
 {
@@ -16,29 +17,40 @@ namespace func
         }
 
         [Function("GetProductDetailsFunction")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req, [BlobInput("products/{productId}.json", Connection ="AzureWebProductsStorage")] string? productData)
+        public  async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req, [BlobInput("products/{productId}.json", Connection ="AzureWebProductsStorage")] string? productData)
         {
-            var queryParams = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
-            string? productId = queryParams["productId"];
-            if (string.IsNullOrEmpty(productId))
+            try
             {
-                var badRequestResponse = req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
-                badRequestResponse.WriteString("Please provide a productId in the query string.");
-                return badRequestResponse;
+                var queryParams = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
+                string? productId = queryParams["productId"];
+                if (string.IsNullOrEmpty(productId))
+                {
+                    var badRequestResponse = req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+                    await badRequestResponse.WriteStringAsync("Please provide a productId in the query string.");
+                    return badRequestResponse;
+                }
+                if (productData == null)
+                { 
+                    var notFoundResponse = req.CreateResponse(System.Net.HttpStatusCode.NotFound);
+                   await notFoundResponse.WriteStringAsync($"Product with ID {productId} not found.");
+                    return notFoundResponse;
+                }
+
+                _logger.LogInformation($"Fetched details for Product ID {productId}");
+
+                var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+                response.Headers.Add("Content-Type", "application/json");
+            await    response.WriteStringAsync(productData);
+                return response;
             }
-            if(productData == null)
+            catch (Exception ex)
             {
-                var notFoundResponse = req.CreateResponse(System.Net.HttpStatusCode.NotFound);
-                notFoundResponse.WriteString($"Product with ID {productId} not found.");
-                return notFoundResponse;
+                _logger.LogError($"Exception occurred: {ex.Message}\n{ex.StackTrace}");
+
+                var errorResponse = req.CreateResponse(HttpStatusCode.InternalServerError);
+               await errorResponse.WriteStringAsync("An error occurred while processing your request.");
+                return errorResponse;
             }
-
-            _logger.LogInformation($"Fetched details for Product ID {productId}");
-
-            var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "application/json");
-            response.WriteString(productData);
-            return response;
         }
     }
 }
